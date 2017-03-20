537f27b90bb5c11962c5e0dd0e3ceb4ee5fa784c
tmobileetcsc <theo.ju26@gmail.com>
2017-03-20 14:17:51 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+// 4 types de fonctions, soit GET, soit POST, soit PUT, soit DELETE
+//GET : on récupère des données
+//POST : on poste des données (on les crées)
+//PUT : on poste aussi des données (mis à jour des données)
+//DELETE : on supprime des données
+
